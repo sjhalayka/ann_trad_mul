05206a0d9d19c43650924b8e3dd0c1877f1f1a08
Shawn Halayka <sjhalayka@gmail.com>
2022-03-03 19:33:40 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -22,10 +22,10 @@ float get_one_to_neg_one(void)
 	return r;
 }
 
-const size_t num_components = 8;
+const size_t num_components = 4;
 const float grid_max = 10;
-const size_t num_training_samples = 10;
-const size_t num_sessions = 10;
+const size_t num_training_samples = 1000000;
+const size_t num_sessions = 1;
 const size_t max_iterations = 10000;
 const float min_error = 0.0001f;
 
@@ -99,13 +99,34 @@ void get_train_data(void)
 	outputTrainingData = Mat(num_training_samples, num_components, CV_32F, outputTrainingDataArray);
 }
 
+template<class T, size_t N>
+vertex<T, N> predict_answer(Ptr<ANN_MLP> &mlp, const vertex<T, N>& in_a, const vertex<T, N>& in_b)
+{
+	float inputArray[num_components * 2];
+
+	for (size_t i = 0; i < num_components; i++)
+		inputArray[i] = in_a.vd[i];
+
+	for (size_t i = num_components; i < num_components * 2; i++)
+		inputArray[i] = in_b.vd[i - num_components];	
+
+	Mat sample = Mat(1, num_components * 2, CV_32F, inputArray);
+	Mat result;
+	mlp->predict(sample, result);
+
+	vertex<T, N> output;
+
+	for (size_t i = 0; i < output.vd.size(); i++)
+		output.vd[i] = result.at<float>(0, static_cast<int>(i));
+
+	return output;
+}
+
 
 
 int main(void)
 {
 	srand(1234);
-
-
 
 	Ptr<ANN_MLP> mlp = ANN_MLP::create();
 
@@ -122,36 +143,63 @@ int main(void)
 
 	mlp->setTrainMethod(ANN_MLP::TrainingMethods::BACKPROP);
 
+	cout << "Session " << 1 << endl;
+
 	get_train_data();
 	Ptr<TrainData> trainingData = TrainData::create(inputTrainingData, SampleTypes::ROW_SAMPLE, outputTrainingData);
 	mlp->train(trainingData);
 
-	for (size_t i = 0; i < num_sessions - 1; i++)
+	for (size_t i = 1; i < num_sessions; i++)
 	{
+		cout << "Session " << i + 1 << endl;
+
 		get_train_data();
 		trainingData = TrainData::create(inputTrainingData, SampleTypes::ROW_SAMPLE, outputTrainingData);
 		mlp->train(trainingData, ANN_MLP::TrainFlags::UPDATE_WEIGHTS | ANN_MLP::TrainFlags::NO_INPUT_SCALE | ANN_MLP::TrainFlags::NO_OUTPUT_SCALE);
 	}
 
 
-	/*cv::FileStorage fs;
+	cv::FileStorage fs;
 	fs.open("mlp.xml", cv::FileStorage::WRITE);
 	mlp->write(fs);
 	fs.release();
 
-	Ptr<ANN_MLP> mlp2 = ANN_MLP::create();
-	fs.open("mlp.xml", cv::FileStorage::READ);
-	mlp2->read(fs.root());
-	fs.release();*/
+	//Ptr<ANN_MLP> mlp2 = ANN_MLP::create();
+	//fs.open("mlp.xml", cv::FileStorage::READ);
+	//mlp2->read(fs.root());
+	//fs.release();
 
-	for (int i = 0; i < inputTrainingData.rows; i++)
-	{
-		Mat sample = Mat(1, inputTrainingData.cols, CV_32F, inputTrainingDataArray[i]);
-		Mat result;
-		mlp->predict(sample, result);
-		cout << sample << " -> " << result << endl;
-		//cout << endl;
-	}
+	vertex<float, num_components> in_a, in_b;
+
+	for (size_t i = 0; i < in_a.vd.size(); i++)
+		in_a.vd[i] = get_one_to_neg_one() * grid_max;
+
+	for (size_t i = 0; i < in_b.vd.size(); i++)
+		in_b.vd[i] = get_one_to_neg_one() * grid_max;
+
+	vertex<float, num_components> answer = predict_answer(mlp, in_a, in_b);
+
+	for (size_t i = 0; i < in_a.vd.size(); i++)
+		cout << in_a.vd[i] << " ";
+
+	cout << endl;
+
+	for (size_t i = 0; i < in_b.vd.size(); i++)
+		cout << in_b.vd[i] << " ";
+
+	cout << endl;
+
+	for (size_t i = 0; i < answer.vd.size(); i++)
+		cout << answer.vd[i] << " ";
+
+	cout << endl;
+
+	vertex<float, num_components> trad_answer = traditional_mul(in_a, in_b);
+
+	for (size_t i = 0; i < trad_answer.vd.size(); i++)
+		cout << trad_answer.vd[i] << " ";
+
+	cout << endl;
 
 	return 0;
 }
